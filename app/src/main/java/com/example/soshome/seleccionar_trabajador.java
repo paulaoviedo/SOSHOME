fdf2d442ff6fc7bafb0e0a96c4b0829e46563101
CarolMoreno <carol.moreno1@correo.usa.edu.co>
2019-02-28 23:07:56 -0500

    Vistas cliente

---
+++ app/src/main/java/com/example/soshome/seleccionar_trabajador.java
@@ -0,0 +1,14 @@
+package com.example.soshome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class seleccionar_trabajador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccionar_trabajador);
+    }
+}
